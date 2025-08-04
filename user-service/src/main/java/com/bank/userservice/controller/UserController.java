d6b43d8c0357f3e924176c792790282851365c09
Anusha Bhat <130855041+anushhabhat@users.noreply.github.com>
2025-08-04 15:14:15 +0530

    Add files via upload

---
+++ user-service/src/main/java/com/bank/userservice/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.bank.userservice.controller;
+
+
+
+import java.util.logging.Logger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.bank.common.dto.AuthRequest;
+import com.bank.common.dto.AuthResponse;
+import com.bank.common.dto.SignUpRequest;
+import com.bank.common.dto.UserDTO;
+import com.bank.userservice.service.UserService;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+
+@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserDTO> signUp(@Valid @RequestBody SignUpRequest signUpRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(signUpRequest));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUser(@PathVariable(name="id") Long id) {
+        return ResponseEntity.ok(userService.getUser(id));
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> signIn(@Valid @RequestBody AuthRequest authRequest) {
+        System.out.println("Authentication request: " + authRequest);
+        return ResponseEntity.ok(userService.authenticate(authRequest));
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable(name="id") Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
